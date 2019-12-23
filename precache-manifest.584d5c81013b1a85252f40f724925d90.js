ebf9cfa177ad63d097fd8e3cea9220eec8e7032a
Joy Dutta <dutta_joy@hotmail.com>
2019-12-23 20:14:30 +0000

    Updates

---
+++ precache-manifest.584d5c81013b1a85252f40f724925d90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13116cefa7d6b877429f09338a7b2826",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "078b402509db070e6ef5",
+    "url": "/monsters-rolodex/static/css/main.665e0665.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "078b402509db070e6ef5",
+    "url": "/monsters-rolodex/static/js/main.ab3469d6.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monsters-rolodex/static/media/logo.25bf045c.svg"
+  }
+]);
